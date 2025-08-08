920d8c147d152ba66df868fd34ae615204d1ed35
chiragfulfagar12 <chiragfulfagar1@gmail.com>
2025-08-08 22:30:02 +0530

    Update 2_Tableau_Dashboard.py

--- Frontend/pages/2_Tableau_Dashboard.py
+++ Frontend/pages/2_Tableau_Dashboard.py
@@ -12,7 +12,8 @@ This page displays an interactive Tableau dashboard providing insights into loan
 # Use the extracted embed URL here
 tableau_embed_url = "https://public.tableau.com/views/CDAC_Project_Dashboard/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link&:embed=y&:showVizHome=no&:tabs=yes&:toolbar=yes"
 
-st.components.v1.iframe(tableau_embed_url, height=800, scrolling=True)
+st.components.v1.iframe(tableau_embed_url, height=1000,width=1200, scrolling=True)
 
 st.markdown("---")
 st.markdown("Dashboard created using Tableau.")
+
